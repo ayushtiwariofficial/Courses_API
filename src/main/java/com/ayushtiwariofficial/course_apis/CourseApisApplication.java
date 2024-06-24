049167f30fd815138a8bfbab7686c29d4729e10b
Ayush Tiwari <ayushtiwari615@gmail.com>
2024-06-24 11:52:48 +0530

    setting up project

---
+++ src/main/java/com/ayushtiwariofficial/course_apis/CourseApisApplication.java
@@ -0,0 +1,15 @@
+package com.ayushtiwariofficial.course_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApisApplication.class, args);
+
+		System.out.println("project started");
+	}
+
+}
---
+++ src/test/java/com/ayushtiwariofficial/course_apis/CourseApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushtiwariofficial.course_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
